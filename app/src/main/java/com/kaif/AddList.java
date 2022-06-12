d3979fbd2f05a9f8b5e75c458c87320018eec7bf
kaif akhtar <kaifakhtar66@gmail.com>
2022-06-12 14:49:23 +0530

    AddList activity open on 'next button' click through intent

--- app/src/main/java/com/kaif/AddList.java
+++ app/src/main/java/com/kaif/AddList.java
@@ -1,4 +1,14 @@
 package com.kaif;
 
-public class AddList {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddList extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_list);
+    }
 }
--- app/src/main/java/com/kaif/MainActivity.java
+++ app/src/main/java/com/kaif/MainActivity.java
@@ -2,7 +2,11 @@ package com.kaif;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button next=findViewById(R.id.next);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, AddList.class);
+        //EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+      // String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
