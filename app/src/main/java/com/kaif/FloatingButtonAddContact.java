b19defc8b8c2939a63efa7d617ea1849087ef041
kaif akhtar <kaifakhtar66@gmail.com>
2022-06-17 01:41:28 +0530

    created a class of FloatingButtonAddContact

---
+++ app/src/main/java/com/kaif/FloatingButtonAddContact.java
@@ -0,0 +1,14 @@
+package com.kaif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FloatingButtonAddContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_button_add_contact);
+    }
+}
