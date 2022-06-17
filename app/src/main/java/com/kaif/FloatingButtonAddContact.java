f9bb8074ed369d974b2722af5efdd68a520750d1
kaif akhtar <kaifakhtar66@gmail.com>
2022-06-17 16:53:47 +0530

    Implemented floating action button which calls the contact app and send the name and phone number

--- app/src/main/java/com/kaif/FloatingButtonAddContact.java
+++ app/src/main/java/com/kaif/FloatingButtonAddContact.java
@@ -2,13 +2,66 @@ package com.kaif;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FloatingButtonAddContact extends AppCompatActivity {
-
+    private static final int RESULT_PICK_CONTACT =1;
+    TextView tx;
+    View fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_floating_button_add_contact); //
+        setContentView(R.layout.activity_floating_button_add_contact);
+
+         fab =findViewById(R.id.floatingActionButton);
+            tx=findViewById(R.id.contactText);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent (Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult (in, RESULT_PICK_CONTACT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Failed To pick contact", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+
+        try {
+            String phoneNo = null;
+            String name = null;
+            Uri uri = data.getData ();
+            cursor = getContentResolver ().query (uri, null, null,null,null);
+            cursor.moveToFirst ();
+            int phoneIndex = cursor.getColumnIndex (ContactsContract.CommonDataKinds.Phone.NUMBER);
+            int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString (phoneIndex);
+            name = cursor.getString (nameIndex);
+            tx.setText (name);
+
+
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
     }
 }
--- app/src/main/java/com/kaif/PriorityButtons.java
+++ app/src/main/java/com/kaif/PriorityButtons.java
@@ -2,7 +2,9 @@ package com.kaif;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PriorityButtons extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PriorityButtons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_priority_buttons);
+    }
+
+    public void priority1_clicked(View view) {
+        Intent intent = new Intent(this, FloatingButtonAddContact.class);
+        startActivity(intent);
     }
 }
