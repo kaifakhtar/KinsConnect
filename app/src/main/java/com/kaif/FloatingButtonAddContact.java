761732997feefdd9052a3ca9ba039a18987b4ac8
kaif akhtar <kaifakhtar66@gmail.com>
2022-06-17 18:29:07 +0530

    Added fully functional chipGroup for displaying selected contacts

--- app/src/main/java/com/kaif/FloatingButtonAddContact.java
+++ app/src/main/java/com/kaif/FloatingButtonAddContact.java
@@ -1,27 +1,33 @@
 package com.kaif;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class FloatingButtonAddContact extends AppCompatActivity {
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+public class FloatingButtonAddContact extends AppCompatActivity implements View.OnClickListener {
     private static final int RESULT_PICK_CONTACT =1;
     TextView tx;
     View fab;
+    ChipGroup chipGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_button_add_contact);
-
+        chipGroup=findViewById(R.id.chipgroup);
          fab =findViewById(R.id.floatingActionButton);
-            tx=findViewById(R.id.contactText);
+            //tx=findViewById(R.id.contactText);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,11 +63,31 @@ public class FloatingButtonAddContact extends AppCompatActivity {
             int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
             phoneNo = cursor.getString (phoneIndex);
             name = cursor.getString (nameIndex);
-            tx.setText (name);
+           // tx.setText (name);
+            //Chip chip=new Chip(this);
+            LayoutInflater inflater = LayoutInflater.from(this);
 
+            // Create a Chip from Layout.
+            Chip chip = (Chip) inflater.inflate(R.layout.layout_entry_chip, this.chipGroup, false);
+            chip.setText(name);
+            chip.setChipIcon(ContextCompat.getDrawable(this,R.drawable.person));
+            chip.setCloseIconVisible(true);
+            chip.setClickable(false);
+            chip.setCheckable(false);
+            chip.setOnCloseIconClickListener(this);
+
+            chipGroup.addView(chip);
+            this.chipGroup.addView(chip);
+            chipGroup.setVisibility(View.VISIBLE);
 
         } catch (Exception e) {
             e.printStackTrace ();
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Chip chip=(Chip) view;
+        chipGroup.removeView(chip);
     }
 }
